1f0f6615b433c6b86a84290ee68d5d1f57efc330
Savannah Scott <sascott624@gmail.com>
2016-10-09 16:15:02 -0400

    added writing sample genres

--- app/assets/javascripts/create/create.controller.js
+++ app/assets/javascripts/create/create.controller.js
@@ -1,14 +1,17 @@
 var CreateController = function(CreateService){
   var vm = this;
 
-  var genres = ["story", "poem", "dialogue"];
-
-  vm.quantity = Math.floor(Math.random() * genres.length) + 1
-
+  var genres = ["story", "poem", "dialogue", "joke", "summary", "introduction", "scene", "song", "tribute", "law", "high school rule", "tardy excuse", "doctor's note", "confession"];
+  vm.quantity = Math.floor(Math.random() * genres.length)
   vm.genre = genres[vm.quantity];
 
   vm.creation = "";
   vm.word = '';
+
+  vm.newGenre = function(){
+    vm.newQuantity = Math.floor(Math.random() * genres.length);
+    vm.genre = genres[vm.newQuantity];
+  }
 
   vm.newWord = function(){
     CreateService.getWord().then(function(response){
