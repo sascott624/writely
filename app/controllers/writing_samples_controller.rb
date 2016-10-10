4611aef1953eb2a953ed1adf7bd77b388e6d0c7f
Savannah Scott <sascott624@gmail.com>
2016-10-10 16:31:04 -0400

    created writing_samples controller, added writing_samples routes

---
+++ app/controllers/writing_samples_controller.rb
@@ -0,0 +1,32 @@
+class WritingSamplesController < ApplicationController
+
+  def index
+    @writing_samples = WritingSample.all
+    render json: @writing_samples
+  end
+
+  def create
+    @writing_sample = WritingSample.create(writing_sample_params)
+    if @writing_sample.save
+      render json: @writing_sample
+    else
+      @writing_sample.errors.add("Error")
+    end
+  end
+
+  def new
+    @writing_sample = WritingSample.new
+  end
+
+  def show
+    @writing_sample = WritingSample.find(params[:id])
+    render json: @writing_sample
+  end
+
+  private
+
+  def writing_sample_params
+    params.require(:writing_sample).permit(:content, :author, :genre)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'writely#index'
+
+  resources :writing_samples, only: [:index, :create, :new, :show]
 end
