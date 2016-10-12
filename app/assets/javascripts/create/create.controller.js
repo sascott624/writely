0fac33bf64264b26b243cedd94e69ae9e46f6b35
Savannah Scott <sascott624@gmail.com>
2016-10-12 15:34:26 -0400

    create.controller safe dependency injection

--- app/assets/javascripts/create/create.controller.js
+++ app/assets/javascripts/create/create.controller.js
@@ -1,11 +1,4 @@
-angular
-  .module('app')
-  .controller('CreateController', ['CreateService', '$state', '$timeout', function(CreateService, $state, $timeout){
-
-
-
-
-// var CreateController = function(CreateService, $state, $timeout){
+var CreateController = function(CreateService, $state, $timeout){
   var vm = this;
 
   var genres = ["story", "poem", "dialogue", "joke", "summary", "introduction", "scene", "song", "tribute", "law", "high school rule", "tardy excuse", "doctor's note", "confession", "speech", "memoir", "lie", "greeting card", "movie trailer voiceover"];
@@ -47,5 +40,10 @@ angular
     $timeout(function(){modal.hide(), vm.refresh()}, 500);
     $timeout(function(){UIkit.modal.alert("Thank you for your submission!")}, 1000);
   }
-
 }
+
+CreateController.$inject = ['CreateService', '$state', '$timeout'];
+
+angular
+  .module('app')
+  .controller('CreateController', CreateController)
